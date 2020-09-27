64054a20585009b4e6b768d89bc4751504a2b21e
Juan Gremes <gremjua@gmail.com>
2020-09-26 21:10:57 -0300

    highlight squares that caused the win

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square (props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} style={{fontWeight: props.bold?'bold':'normal'}}>
       {props.value}
     </button>
   );
@@ -18,6 +18,7 @@ class Board extends React.Component {
         key={i}
         value={this.props.squares[i]} 
         onClick={() => this.props.onClick(i)}
+        bold={this.props.boldSquares?.includes(i)}
       />
     );
   }
@@ -105,7 +106,7 @@ class Game extends React.Component {
     });
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winner.player;
     } else {
       status = `Next player: ${this.state.xIsNext?'X':'O'}`;
     }
@@ -115,6 +116,7 @@ class Game extends React.Component {
           <Board 
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            boldSquares={winner?.squares}
           />
         </div>
         <div className="game-info">
@@ -151,7 +153,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {player: squares[a], squares: lines[i]};
     }
   }
   return null;
